5ba0910506036cdf3cc4296d04d07a93b641a5f3
Cathella <ncathie214@gmail.com>
2024-05-13 10:20:12 +0300

    Working on modal

--- src/pages/Transactions.js
+++ src/pages/Transactions.js
@@ -2,11 +2,30 @@ import { Link } from "react-router-dom";
 import TopNav from "../components/TopNav";
 import { Plus, EllipsisVertical } from 'lucide-react';
 import { useState, useEffect } from 'react';
+import CreateTransaction from "../components/CreateTransaction";
 
 function Transactions() {
   const [transactions, setTransactions] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    console.log("Opening modal");
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    console.log('Closing modal');
+    setIsModalOpen(false);
+  };
+
+  const handleSubmit = (formData) => {
+    // Logic to handle form submission (e.g., send data to backend)
+    console.log('Form data:', formData);
+    // Close modal after submitting
+    closeModal();
+  };
 
   useEffect(() => {
     const fetchTransactions = async () => {
@@ -39,45 +58,49 @@ function Transactions() {
       <div className="top-content">
         <div className="title">
           <h2>Transactions</h2>
-          <Link to="/" className="create-btn"><Plus /><span>Create Transaction</span></Link>
+          <button onClick={openModal} className="create-btn"><Plus /><span>Create Transaction</span></button>
         </div>
+        {isModalOpen && (
+          <CreateTransaction isOpen={isModalOpen} onClose={closeModal} onSubmit={handleSubmit} />
+        )}
         {loading ? (
           <p>Loading...</p>
         ) : error ? (
           <p>Error: {error}</p>
         ) : (
           <table className="transactions-table">
-          <thead>
-            <tr>
-              {/* <th className="caption">ID</th> */}
-              <th className="caption">Ref Number</th>
-              <th className="caption">Account to</th>
-              <th className="caption">Account from</th>
-              <th className="caption date">Date</th>
-              <th className="caption amount">Amount</th>
-              <th className="caption actions">Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {transactions.map(transaction => (
-              <tr key={transaction.id}>
-                {/* <td>{transaction.id}</td> */}
-                <td>{transaction.tranReference}</td>
-                <td>{transaction.accountToId}</td>
-                <td>{transaction.accountFromId}</td>
-                <td className="date">{formatDate(transaction.transactionDate)}</td>
-                <td className="amount">{transaction.amount}</td>
-                <td className="actions">
-                  <Link to="/" className="more">
-                    <EllipsisVertical />
-                  </Link>
-                </td>
+            <thead>
+              <tr>
+                {/* <th className="caption">ID</th> */}
+                <th className="caption">Ref Number</th>
+                <th className="caption">Account to</th>
+                <th className="caption">Account from</th>
+                <th className="caption date">Date</th>
+                <th className="caption amount">Amount</th>
+                <th className="caption actions">Actions</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {transactions.map(transaction => (
+                <tr key={transaction.id}>
+                  {/* <td>{transaction.id}</td> */}
+                  <td>{transaction.tranReference}</td>
+                  <td>{transaction.accountToId}</td>
+                  <td>{transaction.accountFromId}</td>
+                  <td className="date">{formatDate(transaction.transactionDate)}</td>
+                  <td className="amount">{transaction.amount}</td>
+                  <td className="actions">
+                    <Link to="/" className="more">
+                      <EllipsisVertical />
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         )}
       </div>
+      {/* <CreateTransaction isOpen={isModalOpen} onClose={closeModal} onSubmit={handleSubmit} /> */}
     </div>
   );
 }
